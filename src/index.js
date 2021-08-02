67898f4c1ffc15721fb9e58e5323f9a2c2cc6e9d
Felipe Estevão Moreno <contato@wr5solutions.com.br>
2021-08-01 21:52:11 -0300

    build: challenge complete

--- src/index.js
+++ src/index.js
@@ -10,19 +10,64 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find( user => user.username === username );
+
+  if( !user ) {
+    return response.status(404).json({ error: `User ${username} not found.`})
+  }
+
+  request.user = user;
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if ( !user.pro && user.todos.length === 10 ){
+    return response.status(403).json({ error: "User is not Pro and already have ten todos"});
+  }
+
+  next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find( user => user.username === username );
+  if( !user ) {
+    return response.status(404).json({ error: `User ${username} not found.`})
+  }
+  
+  if(!validate(id)) {
+    return response.status(400).json({ error: "Id is not a valid uuid."});
+  }
+
+  const todo = user.todos.find( todo => todo.id === id );
+
+  if( !todo ) {
+    return response.status(404).json({ error: `Todo id not found.`})
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find( user => user.id === id );
+  
+  if( !user ){
+    return response.status(404).json({ error: "User id not found."});
+  }
+  
+  request.user = user;
+  next();
 }
 
 app.post('/users', (request, response) => {
